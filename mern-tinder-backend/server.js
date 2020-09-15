ad79c49e431957c9d517daf5464699cddab38395
Patrick Wehbe <pew02@mail.aub.edu>
2020-09-15 18:12:29 +0300

    api endpoints and middlewares

--- mern-tinder-backend/server.js
+++ mern-tinder-backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import Cards from "./dbCards.js";
+import Cors from "cors";
 
 //App Config
 const app = express();
@@ -9,15 +11,41 @@ const connection_url =
 
 //middlewares
 
+app.use(express.json());
+app.use(Cors());
+
 //DB Config
 mongoose.connect(connection_url, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
+
 //API Endpoints
 
 app.get("/", (req, res) => res.status(200).send("Hello, world"));
+
+app.post("/tinder/cards", (req, res) => {
+  const dbCard = req.body;
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/tinder/cards", (req, res) => {
+  const dbCard = req.body;
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 //Listeners
 
