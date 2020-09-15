bdd28ef1d32e2ee07c909bd85831719a924d3320
Patrick Wehbe <pew02@mail.aub.edu>
2020-09-15 17:07:04 +0300

    footer init

--- mern-tinder-frontend/src/App.js
+++ mern-tinder-frontend/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import Cards from "./TinderCards";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Cards />
-      {/* images */}
-      {/* Footer */}
+      <Footer />
     </div>
   );
 }
