e25fcf3a037bd6db7fa481a61eec4daba30ceff1
Patrick Wehbe <pew02@mail.aub.edu>
2020-09-15 15:49:42 +0300

    added material UI and Header

---
+++ mern-tinder-frontend/src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import IconButton from "@material-ui/core/IconButton";
+
+function Header() {
+  return (
+    <div>
+      <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
+    </div>
+  );
+}
+
+export default Header;
