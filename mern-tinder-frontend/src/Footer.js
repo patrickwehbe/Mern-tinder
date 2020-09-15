fb5eaf8b14c3a30a67c0d7213c3fae470dca313f
Patrick Wehbe <pew02@mail.aub.edu>
2020-09-15 17:21:44 +0300

    footer

--- mern-tinder-frontend/src/Footer.js
+++ mern-tinder-frontend/src/Footer.js
@@ -1,8 +1,32 @@
 import React from "react";
 import "./Footer.css";
+import ReplayIcon from "@material-ui/icons/Replay";
+import CloseIcon from "@material-ui/icons/Close";
+import StarRateIcon from "@material-ui/icons/StarRate";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+import IconButton from "@material-ui/core/IconButton";
 
 function Footer() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <IconButton className="footer__repeat">
+        <ReplayIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="footer__left">
+        <CloseIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="footer__star">
+        <StarRateIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="footer__right">
+        <FavoriteIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="footer__lightning">
+        <FlashOnIcon fontSize="large" />
+      </IconButton>
+    </div>
+  );
 }
 
 export default Footer;
